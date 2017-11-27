97dacb5e2a62f6055811c6ed68e724fd27bf6ed1
maheshshastri <shastrimahesh1@gmail.com>
2017-11-27 13:04:45 +0530

    Removed unwanted text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello World!! I am back");
+  res.send("Hello World!!");
 });
 
 app.listen(4000);
